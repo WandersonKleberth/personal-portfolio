b44277e1bce4970f5a3cb421ab4ce30f7b00a99f
WandersonKlebert <wandersondevbeginner@gmail.com>
2024-09-11 23:29:25 -0300

    att efects page

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.firstSection', {duration: 2500});
+
+sr.reveal('.secondSection', {
+    // rotate: {x:0, y:80, z:0},
+    duration: 2000
+});
+
+sr.reveal('.thirdSection', {
+    // rotate: {x:100, y:100, z:0},
+    duration: 2000
+})
+
